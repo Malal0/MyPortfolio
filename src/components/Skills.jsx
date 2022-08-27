c97f0a49001221b76a63baaa639f9137a8235866
UncomfortableChai <mj1055119@gmail.com>
2022-08-26 17:28:35 -0700

    working on the skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
-import Logo from '../assets/images/JEM-Logo.svg'
-import ProPic from '../assets/images/ProfilePic.png'
-import GithubIcon from '../assets/images/githubicon.svg'
-import WeblinkIcon from '../assets/images/weblinkicon.svg'
+import Htmllogo from '../assets/images/html5logo.png'
+import Csslogo from '../assets/images/csslogo.png'
+import Javascriptlogo from '../assets/images/javascriptlogo.png'
+import Photoshoplogo from '../assets/images/photoshoplogo.png'
+import Reactlogo from '../assets/images/reactlogo.png'
+import Figmalogo from '../assets/images/figmalogo.png'
+import Blenderlogo from '../assets/images/blenderlogo.png'
 
 function Skills() {
     return (
         <div className='Skills-Section'>
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
-            <img src={Logo} className="logo" alt="Logo" />
+            <div className='skill'>
+                <img src={Htmllogo} alt="Html logo" />
+                <p>html</p>
+            </div>
+            <div className='skill'>
+                <img src={Csslogo} alt="Css logo" />
+                <p>css</p>
+            </div>
+            <div className='skill'>
+                <img src={Javascriptlogo} alt="Javascript logo" />
+                <p>javascript</p>
+            </div>
+            <div className='skill'>
+                <img src={Photoshoplogo} alt="Photoshop logo" />
+                <p>photoshop</p>
+            </div>
+            <div className='skill'>
+                <img src={Reactlogo} alt="React logo" />
+                <p>react</p>
+            </div>
+            <div className='skill'>
+                <img src={Figmalogo} alt="Figma logo" />
+                <p>figma</p>
+            </div>
+            <div className='skill'>
+                <img src={Blenderlogo} alt="Blender logo" />
+                <p>blender</p>
+            </div>
         </div>
     )
 }
