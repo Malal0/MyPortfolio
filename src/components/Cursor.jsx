3d1db334bc77e615140e55f0f0b48b1c24a8dc21
UncomfortableChai <mj1055119@gmail.com>
2022-08-29 23:58:57 -0700

    working on cursor functionality

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import './cursor.css'
 
 function Cursor() {
+    const cursor = document.querySelector('.Cursor');
+
+    addEventListener('mousemove', e => {
+        // console.log(e.clientX, e.clientY);
+        cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
+        // console.log(cursor.clientLeft, cursor.clientTop);
+    })
 
     return (
         <div className="Cursor">
