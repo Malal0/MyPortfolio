4694ddb57daf9ca4d96ad46b03a8710999f1c19c
UncomfortableChai <mj1055119@gmail.com>
2022-08-31 23:10:14 -0700

    got the custom cursor to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import Logo from './assets/images/JEM-Logo.svg'
-import ProPic from './assets/images/ProfilePic.png'
-import GithubIcon from './assets/images/githubicon.svg'
-import WeblinkIcon from './assets/images/weblinkicon.svg'
-import LinkedinIcon from './assets/images/linkedinlogo.svg'
-import ToTopIcon from './assets/images/toppagebtn.svg'
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
@@ -12,11 +7,45 @@ import AboutMe from './components/AboutMe'
 import MyArtwork from './components/MyArtwork'
 import Contact from './components/Contact'
 import TopOfPageBtn from './components/TopOfPageBtn'
-import Cursor from './components/Cursor'
+// import Cursor from './components/Cursor'
+import './components/cursor.css'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // const [] = useState({ x: 0, y: 0 });
+
+  const cursor = document.querySelector('.Cursor');
+
+  //  code here
+
+  const [cursorCords, setCursorCords] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    const mouseMove = e => {
+      setCursorCords({
+        x: e.clientX - cursor.clientHeight / 2,
+        y: e.clientY - cursor.clientWidth / 2
+      })
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, []);
+
+  const variants = {
+    default: {
+      x: cursorCords.x,
+      y: cursorCords.y
+    }
+  }
+
+  //  code here
 
   return (
     <div className='App'>
@@ -27,7 +56,12 @@ function App() {
       <MyArtwork />
       <Contact />
       <TopOfPageBtn />
-      <Cursor />
+      {/* <motion.Cursor /> */}
+      <motion.div
+        className='Cursor'
+        variants={variants}
+        animate="default"
+      />
     </div>
   )
 }
--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -2,18 +2,8 @@ import React from 'react'
 import './cursor.css'
 
 function Cursor() {
-    const cursor = document.querySelector('.Cursor');
-
-    addEventListener('mousemove', e => {
-        cursor.style.top = `${e.pageY - cursor.clientHeight / 2}px`
-        cursor.style.left = `${e.pageX - cursor.clientWidth / 2}px`
-
-        console.log(e.pageX, e.pageY);
-    })
-
     return (
-        <div className="Cursor">
-        </div>
+        <div className="Cursor" />
     )
 }
 
