6175ab7af3d692a4050b03d053014bb7d8efea34
UncomfortableChai <mj1055119@gmail.com>
2022-09-04 23:06:10 -0700

    updated contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,24 +4,42 @@ import LinkedinIcon from '../assets/images/linkedinlogo.svg'
 import './contact.css'
 
 function Contact() {
+    const name = document.getElementById('fullName');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+    const info = {
+        fullName: '',
+        email: '',
+        message: ''
+    }
+
+    const getMessage = () => {
+        console.log(name.value, email.value, message.value);
+        // name.value = '';
+        // email.value = '';
+        // message.value = '';
+    }
+
     return (
         <div className='Contact-Section'>
             <h3>contact me</h3>
-            <form>
+            <form action="josue_mar@protonmail.com"
+                method="POST"
+                enctype="multipart/form-data">
                 <div className='flex-r'>
                     <div className='flex-c'>
                         <label>full name</label>
-                        <input type='text' placeholder='YOUR NAME'></input>
+                        <input type='text' placeholder='YOUR NAME' id='fullName'></input>
                     </div>
                     <div className='flex-c'>
                         <label>email</label>
-                        <input type='text' placeholder='EMAIL'></input>
+                        <input type='email' placeholder='EMAIL' id='email' required></input>
                     </div>
                 </div>
                 <div className='flex-c'>
                     <label>message</label>
                     <div className='message-container'>
-                        <textarea className='message' placeholder='Hello Dev!'></textarea>
+                        <textarea className='message' placeholder='Hello Dev!' id='message'></textarea>
                     </div>
                 </div>
             </form>
@@ -31,7 +49,7 @@ function Contact() {
                     <a href='https://www.linkedin.com/in/josu%C3%A9-martinez-web-developer/' target='_blank'><img src={LinkedinIcon} className="logo-link" alt="LinkedIn link" /></a>
                 </div>
                 {/* <div> */}
-                <button>send</button>
+                <button onClick={getMessage} type="submit" value="Submit">send</button>
                 {/* </div> */}
             </div>
         </div>
